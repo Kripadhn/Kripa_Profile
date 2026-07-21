e9bf3638008bf3b67e48b90e1057765b6a083b31
Kripa Shankar <51108253+Kripadhn@users.noreply.github.com>
2026-07-22 03:53:26 +0530

    Change pathPrefix in gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 const config = require('./src/config');
 
 module.exports = {
-  pathPrefix: '/Kripa_Profile.github.io',
+  pathPrefix: '/Kripa_Profile',
   siteMetadata: {
     title: config.siteTitle,
     siteUrl: config.siteUrl,
