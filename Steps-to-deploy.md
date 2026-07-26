07ae970f0c5d680d0c76fc6d86d464beca7e1f0d
Kripadhn <krips.dhn@gmail.com>
2026-07-27 02:46:52 +0530

    Index and steps to deploy changes

--- Steps-to-deploy.md
+++ Steps-to-deploy.md
@@ -2,7 +2,7 @@
 
   - git clone
 
-  - cd chandrikadeb7.github.io
+  - cd kripadhn.github.io/Kripa_Profile/
 
   - source ~/.nvm/nvm.sh
 
--- src/pages/pensieve/index.js
+++ src/pages/pensieve/index.js
@@ -123,8 +123,8 @@ const PensievePage = ({ location, data }) => {
   return (
     <Layout location={location}>
       <Helmet>
-        <title>Pensieve | Chandrika Deb</title>
-        <link rel="canonical" href="https://chandrikadeb7.github.io/pensieve" />
+        <title>Pensieve | Kripa Shankar</title>
+        <link rel="canonical" href="https://kripadhn.github.io/Kripa_Profile/" />
       </Helmet>
 
       <StyledMainContainer>
